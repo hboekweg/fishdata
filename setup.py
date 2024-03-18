9fd8f7507064a80e4afcee8fdc52406c65845587
hboekweg <hannahboekweg@gmail.com>
2024-03-18 09:43:29 -0600

    add pyproject.toml for new pypi dist system

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "lakepowell",
-    version="0.1.2",
+    version="0.1.9",
     author="lakepowellapi",
     author_email="lakepowellapi@gmail.com",
     description="lake powell Python package",
